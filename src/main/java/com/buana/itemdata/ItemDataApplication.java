2173c82039534cd8abf0fd0825655d569e4b6dce
Ade.Rahayu <adeintanrahayu.business@gmail.com>
2023-03-09 11:42:06 +0700

    first commit

---
+++ src/main/java/com/buana/itemdata/ItemDataApplication.java
@@ -0,0 +1,13 @@
+package com.buana.itemdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDataApplication.class, args);
+	}
+
+}
